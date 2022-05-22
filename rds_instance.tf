fc0f5c03c99d3a8c510b9207b9f6a33e42255eac
Menna97 <menna.elgedawy9761@gmail.com>
2022-05-22 14:50:12 +0200

    update database security group

--- rds_instance.tf
+++ rds_instance.tf
@@ -10,4 +10,5 @@ resource "aws_db_instance" "rds" {
   skip_final_snapshot  = true
   db_subnet_group_name = aws_db_subnet_group.db-subnet.name
   publicly_accessible  = true
-}
+  vpc_security_group_ids = [aws_security_group.rds_sg.id]
+} 
--- security_group.tf
+++ security_group.tf
@@ -18,6 +18,38 @@ resource "aws_security_group" "bastion_sg" {
     protocol    = "tcp"
   }
 
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    description = "HTTP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    description = "HTTPS"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    description = "MySQL"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 6379
+    to_port     = 6379
+    protocol    = "tcp"
+    description = "Redis"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   # allow egress of all ports
   egress {
     from_port   = 0
@@ -54,5 +86,47 @@ resource "aws_security_group" "app_sg" {
   tags = {
     Name        = "app_sg"
     Description = "Allow SSH from vpc cidr"
+  }
+}
+
+resource "aws_security_group" "rds_sg" {
+  name = "rds_sg"
+  vpc_id      = module.network.vpc_id
+
+  # Only MySQL in
+  ingress {
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Allow all outbound traffic.
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "ec_sg" {
+  name = "ec_sg"
+  vpc_id      = module.network.vpc_id
+
+  # Only MySQL in
+  ingress {
+    from_port   = 6379
+    to_port     = 6379
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Allow all outbound traffic.
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
